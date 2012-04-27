c944a4745fa156026d4155c0be1b2d296a4312bb
Will Potter <will@wpotter.me>
2012-04-27 13:43:27 -0400

    Glazed Shading Works

--- ray/shader/Glazed.java
+++ ray/shader/Glazed.java
@@ -35,40 +35,49 @@ public class Glazed extends Shader {
     public void shade(Color outColor, Scene scene, ArrayList<Light> lights, Vector3 toEye, 
                       IntersectionRecord record, int depth, double contribution, boolean internal) {
 
-        for (Light li : lights) {
+    		toEye.normalize();
         	//Calculate Reflection Ray
-            Vector3 l = new Vector3();
-            l.sub(li.position, record.location); // unit vector towards light
-            l.normalize();
+    		// r = -toEye - 2(-toEye.dot(n))*n;
+            Vector3 l = new Vector3(toEye);
+            l.scale(-1);
             Vector3 norm = new Vector3(record.normal);
             double d = -2*l.dot(norm);
             norm.scale(d);
-            l.sub(norm);
+            l.add(norm);
             l.normalize();
             Ray reflectionRay = new Ray(record.location,l);
-            
+            reflectionRay.start = Ray.EPSILON;
+            reflectionRay.end = Double.POSITIVE_INFINITY;
+
 	    	if(depth<=MAXDEPTH&&contribution>=MINCONTRIBUTION) {
 
 				// Implement Fresnel equation (Shirley 13.1)
 				// Scale the vector L by -1 to get R that points away from the surface as the normal does.
-				Vector3 r = new Vector3(l);
-				r.scale(-1);
-				r.normalize();
-				
-				double nt = this.refractiveIndex;
-				double r0 = Math.pow(((nt-1)/(nt+1)), 2);
-				double theta = Math.acos(record.normal.dot(r)/(record.normal.length()*r.length()));
-				double reflection = r0 + Math.pow((1-r0)*(1-Math.cos(theta)),5);
-				
-				// WHAT TO DO WITH CONTRIBUTION??????
+				Vector3 r = new Vector3(toEye);
 
+				r.normalize();
+
+				double r0 = Math.pow(((refractiveIndex-1)/(refractiveIndex+1)), 2);
+
+				double theta = Math.acos(record.normal.dot(r));
+				double reflection = r0 + (1-r0)*Math.pow((1-Math.cos(theta)),5);
+				//System.out.println(reflection);
+
+				Color reflectionColor = new Color(0,0,0);
+				Color substrateColor = new Color(0,0,0);
+				
 				// compute reflected contribution (make a recursive call to shadeRay)
-				RayTracer.shadeRay(outColor, scene, reflectionRay, lights, depth, contribution, internal);
+		    	depth++;
+				RayTracer.shadeRay(reflectionColor, scene, reflectionRay, lights, depth, contribution, internal);
 				
 				// compute substrate contribution (call substrate.shade(...))
-				substrate.shade(outColor, scene, lights, toEye, record, depth, contribution, internal);
-		    	depth++;
+				substrate.shade(substrateColor, scene, lights, toEye, record, depth, contribution, internal);
+				
+				reflectionColor.scale(reflection);
+				substrateColor.scale(1-reflection);
+				reflectionColor.add(substrateColor);
+				outColor.set(reflectionColor);
 	    	}
-        }
+	    	
     }
 }
