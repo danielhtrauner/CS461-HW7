388d8b5b1a69db18fc883670c0f892fcf15b77d2
Daniel Trauner <danielhtrauner@gmail.com>
2012-04-26 22:59:21 -0400

    Potentially working AA

--- ray/RayTracer.java
+++ ray/RayTracer.java
@@ -9,51 +9,57 @@ import ray.math.Vector3;
 
 /**
  * A simple ray tracer.
- *
+ * 
  * @author ags, modified by DS 2/2012
  */
 public class RayTracer {
 
 	public static String testFolderPath;
-	
-	public static String getTestFolderPath() { return testFolderPath; }
+
+	public static String getTestFolderPath() {
+		return testFolderPath;
+	}
+
 	/**
-	 * If filename is a directory, set testFolderPath = fn.
-	 * And return a list of all .xml files inside the directory
-	 * @param fn Filename or directory
+	 * If filename is a directory, set testFolderPath = fn. And return a list of
+	 * all .xml files inside the directory
+	 * 
+	 * @param fn
+	 *            Filename or directory
 	 * @return fn itself in case fn is a file, or all .xml files inside fn
 	 */
 	public static final ArrayList<String> getFileLists(String fn) {
-		if(fn.endsWith("/"))
-			fn.substring(0, fn.length()-1);
-		
+		if (fn.endsWith("/"))
+			fn.substring(0, fn.length() - 1);
+
 		File file = new File(fn);
 		ArrayList<String> output = new ArrayList<String>();
-		if(file.exists()) {
-			if(file.isFile()) {
-				if(file.getParent() != null)
+		if (file.exists()) {
+			if (file.isFile()) {
+				if (file.getParent() != null)
 					testFolderPath = file.getParent() + "/";
 				else
 					testFolderPath = "";
 				output.add(fn);
 			} else {
 				testFolderPath = fn + "/";
-				for(String fl : file.list()) {
-					if(fl.endsWith(".xml")) {
+				for (String fl : file.list()) {
+					if (fl.endsWith(".xml")) {
 						output.add(testFolderPath + fl);
 					}
-				}	
+				}
 			}
 		} else
 			System.out.println("File not found.");
 
 		return output;
 	}
+
 	/**
 	 * The main method takes all the parameters an assumes they are input files
 	 * for the ray tracer. It tries to render each one and write it out to a PNG
 	 * file named <input_file>.png.
-	 *
+	 * 
 	 * @param args
 	 */
 	public static final void main(String[] args) {
@@ -62,17 +68,17 @@ public class RayTracer {
 		for (int ctr = 0; ctr < args.length; ctr++) {
 
 			ArrayList<String> fileLists = getFileLists(args[ctr]);
-			
+
 			for (String inputFilename : fileLists) {
 				String outputFilename = inputFilename + ".png";
-	
+
 				// Parse the input file
 				Scene scene = (Scene) parser.parse(inputFilename, Scene.class);
 				System.out.printf("Rendering %-25s  ", inputFilename);
-				
+
 				// Render the scene
 				renderImage(scene);
-	
+
 				// Write the image out
 				scene.getImage().write(outputFilename);
 			}
@@ -81,8 +87,9 @@ public class RayTracer {
 
 	/**
 	 * The renderImage method renders the entire scene.
-	 *
-	 * @param scene The scene to be rendered
+	 * 
+	 * @param scene
+	 *            The scene to be rendered
 	 */
 	public static void renderImage(Scene scene) {
 
@@ -104,26 +111,47 @@ public class RayTracer {
 		int total = height * width;
 		int counter = 0;
 		int lastShownProgress = 0;
-				
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
+		
+		// Allows for 9 samples per pixel
+		scene.setSamples(3);
+		int ns = scene.samples;
+		int nx = width+1;
+		int ny = height+1;
+		int x = 0;
+		int y = 0;
 
-                cam.getRay(ray, (x + 0.5)/(double)width, (y + 0.5)/(double)height);	
+		for (int iy = 0; iy < ny-1; iy++) {
+			for (int ix = 0; ix < nx-1; ix++) {
+
+				Color sum = new Color(0,0,0);
 				
-				shadeRay(rayColor, scene, ray, scene.getLights(), 1, 1, false);
-				pixelColor.set(rayColor);
-				
-				//Gamma correct and clamp pixel values
-				pixelColor.gammaCorrect(2.2);
-				pixelColor.clamp(0, 1);
-				image.setPixelColor(pixelColor, x, y);
-				
-				counter++;
-				float numberOfProgressDots = 20;
-				if((int)(numberOfProgressDots * counter / total) != lastShownProgress) {
-					lastShownProgress = (int)(numberOfProgressDots * counter / total);
-					System.out.print(".");
+				for (int dx = 0; dx < ns-1; dx++) {
+					for (int dy = 0; dy < ns-1; dy++) {
+						x = (int)((ix + (dx + 0.5) / ns) / nx);
+						y = (int)((iy + (dy + 0.5) / ns) / ny);
+						
+						cam.getRay(ray, (x + 0.5)/(double)width, (y + 0.5)/(double)height);	
+						
+						shadeRay(rayColor, scene, ray, scene.getLights(), 1, 1, false);
+						
+						sum.r += rayColor.r;
+						sum.g += rayColor.g;
+						sum.b += rayColor.b;
+						
+					}
+					pixelColor.set(sum);
+					pixelColor.gammaCorrect(2.2);
+					pixelColor.clamp(0, 1);
+					image.setPixelColor(pixelColor, x, y);
 				}
+			}
+
+			// Do all of the pretty stuff (unimportant)
+			counter++;
+			float numberOfProgressDots = 20;
+			if((int)(numberOfProgressDots * counter / total) != lastShownProgress) {
+				lastShownProgress = (int)(numberOfProgressDots * counter / total);
+				System.out.print(".");
 			}
 		}
 
@@ -135,31 +163,38 @@ public class RayTracer {
 
 	/**
 	 * This method returns the color along a single ray in outColor.
-	 *
-	 * @param outColor output space
-	 * @param scene the scene
-	 * @param ray the ray to shade
+	 * 
+	 * @param outColor
+	 *            output space
+	 * @param scene
+	 *            the scene
+	 * @param ray
+	 *            the ray to shade
 	 */
-	public static void shadeRay(Color outColor, Scene scene, Ray ray,// Workspace workspace, 
-			ArrayList<Light> lights, int depth, double contribution, boolean internal) {
-		
+	public static void shadeRay(Color outColor, Scene scene,
+			Ray ray,// Workspace workspace,
+			ArrayList<Light> lights, int depth, double contribution,
+			boolean internal) {
+
 		// Reset the output color
 		outColor.set(0, 0, 0);
 
 		IntersectionRecord eyeRecord = new IntersectionRecord();
 		Vector3 toEye = new Vector3();
 
-        eyeRecord.surface = null;
-        
-        scene.getFirstIntersection(eyeRecord, ray);
-        if (eyeRecord.surface == null)
-            return;
-        toEye.sub(ray.origin, eyeRecord.location);
-        
+		eyeRecord.surface = null;
+
+		scene.getFirstIntersection(eyeRecord, ray);
+		if (eyeRecord.surface == null)
+			return;
+		toEye.sub(ray.origin, eyeRecord.location);
+
 		// Compute color
-		// Hint: while developing and for debugging you might want to set outColor to some other colors, without using shaders. 
+		// Hint: while developing and for debugging you might want to set
+		// outColor to some other colors, without using shaders.
 		if (eyeRecord.surface != null)
-			eyeRecord.surface.getShader().shade(outColor, scene, lights, toEye, eyeRecord, depth, contribution, internal);
+			eyeRecord.surface.getShader().shade(outColor, scene, lights, toEye,
+					eyeRecord, depth, contribution, internal);
 	}
 
 }
